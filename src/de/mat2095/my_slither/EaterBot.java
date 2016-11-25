06362b12c65fbca0827c0fb97ca91f54e59489d4
Mat2095 <Matthias-Bay@t-online.de>
2016-11-26 00:54:29 +0100

    Bot improvements and cleanup

--- src/de/mat2095/my_slither/EaterBot.java
+++ src/de/mat2095/my_slither/EaterBot.java
@@ -20,6 +20,18 @@ public class EaterBot extends Player {
         return result;
     }
 
+    private static double smoothify(double value) {
+        return Math.cos(value * Math.PI) / 2 + 0.5;
+    }
+
+    private void addFoodMapPoint(Food food, int color, double alpha) {
+        MapPoint mapPoint = new MapPoint(16, new Color(color | (int) (alpha * 0x7F) << 24, true));
+        mapPoint.x = food.x;
+        mapPoint.y = food.y;
+        mapPoint.visible = true;
+        mapPoints.add(mapPoint);
+    }
+
     @Override
     public Wish action(MySlitherModel model) { // TODO: don't use model directly
         mapPoints.clear();
@@ -34,39 +46,39 @@ public class EaterBot extends Player {
         if (snake != null) {
             for (Food food : model.foods.values()) {
 
-                double sizeWeight = 1 - Math.pow(64 / (64 + food.getSize()), 4);//TODO: viel zu schwach
+                /*  TODO:
+                 *  - fix shaking?
+                 *  - ignore unreachable food directly behind snake
+                 *  - correlation distance/direction (great distance => direction not important)
+                 *  - ignore food blocked by enemies
+                 *  - given two piles, chose one of them, not the middle
+                 */
+                
+                double sizeWeight = 128 / (128 + Math.pow(food.getRadius(), 1.5));
+                sizeWeight = sizeWeight * sizeWeight;
+                sizeWeight = smoothify(sizeWeight);
 
                 double distance = Math.sqrt((food.x - snake.x) * (food.x - snake.x) + (food.y - snake.y) * (food.y - snake.y));
-                double newDistance = 1 - model.sectorSize / (model.sectorSize + distance);
-                double distanceWeight = Math.cos(newDistance * Math.PI) / 2 + 0.5;
+                double distanceWeight = 1 - model.sectorSize / (model.sectorSize + distance);
+                distanceWeight = smoothify(distanceWeight);
 
                 double angle = Math.atan2(food.y - snake.y, food.x - snake.x);
                 double deltaAngle = angle - snake.ang;
                 if (deltaAngle < -Math.PI) {
                     deltaAngle += MySlitherModel.PI2;
                 }
-                //double directionWeight = 1.25 - Math.abs(deltaAngle) / Math.PI; // gread distance: weight not important
                 double directionWeight = Math.abs(deltaAngle) / Math.PI;
-                directionWeight = Math.cos(directionWeight * Math.PI) / 2.0 + 0.5;
-                directionWeight = Math.pow(directionWeight, 4);
-                directionWeight = 0.9 * directionWeight + 0.1;
+                directionWeight = smoothify(directionWeight);
+                directionWeight = 0.9 * Math.pow(directionWeight, 4) + 0.1;
                 if (distance < model.sectorSize && directionWeight < 0.25) {
                     directionWeight = 0;
                 }
 
-                MapPoint goodFoodAngle = new MapPoint(16, new Color(0, 0xFF, 0x80, (int) (sizeWeight * 0xFF)));
-                goodFoodAngle.x = food.x;
-                goodFoodAngle.y = food.y;
-                goodFoodAngle.visible = true;
-                mapPoints.add(goodFoodAngle);
+                double weight = sizeWeight * distanceWeight * directionWeight;
 
-//                MapPoint goodFoodDistance = new MapPoint(16, new Color(0xFF, 0, 0x80, (int) (distanceWeight * 0xFF)));
-//                goodFoodDistance.x = food.x;
-//                goodFoodDistance.y = food.y;
-//                goodFoodDistance.visible = true;
-//                mapPoints.add(goodFoodDistance);
-                double weight = sizeWeight * distanceWeight * directionWeight; //TODO: sperre nach hinten, gegner dazwischen
-                // zwei haufen: wähle einen, nicht die mitte!
+                addFoodMapPoint(food, 0xFF8000, sizeWeight);
+                addFoodMapPoint(food, 0x00FF80, distanceWeight);
+                addFoodMapPoint(food, 0x8000FF, directionWeight);
 
                 sumX += food.x * weight;
                 sumY += food.y * weight;
