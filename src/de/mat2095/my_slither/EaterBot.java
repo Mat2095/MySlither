13da6d8faf10f831687cc7ef6f82f9d1bda0959e
Mat2095 <Matthias-Bay@t-online.de>
2016-11-25 23:04:54 +0100

    Updated EaterBot

--- src/de/mat2095/my_slither/EaterBot.java
+++ src/de/mat2095/my_slither/EaterBot.java
@@ -8,7 +8,7 @@ public class EaterBot extends Player {
 
     private final MapPoint aim;
 
-    public EaterBot() {
+    private EaterBot() {
         super("EaterBot");
         aim = new MapPoint(16, new Color(0xA000FF00, true));
         mapPoints.add(aim);
@@ -22,6 +22,9 @@ public class EaterBot extends Player {
 
     @Override
     public Wish action(MySlitherModel model) { // TODO: don't use model directly
+        mapPoints.clear();
+        mapPoints.add(aim);
+
         double sumX = 0;
         double sumY = 0;
         double sumWeight = 0;
@@ -30,18 +33,41 @@ public class EaterBot extends Player {
 
         if (snake != null) {
             for (Food food : model.foods.values()) {
+
+                double sizeWeight = 1 - Math.pow(64 / (64 + food.getSize()), 4);//TODO: viel zu schwach
+
                 double distance = Math.sqrt((food.x - snake.x) * (food.x - snake.x) + (food.y - snake.y) * (food.y - snake.y));
+                double newDistance = 1 - model.sectorSize / (model.sectorSize + distance);
+                double distanceWeight = Math.cos(newDistance * Math.PI) / 2 + 0.5;
 
                 double angle = Math.atan2(food.y - snake.y, food.x - snake.x);
                 double deltaAngle = angle - snake.ang;
                 if (deltaAngle < -Math.PI) {
                     deltaAngle += MySlitherModel.PI2;
                 }
-                double directionWeight = 1.25 - Math.abs(deltaAngle) / Math.PI; // gread distance: weight not important
+                //double directionWeight = 1.25 - Math.abs(deltaAngle) / Math.PI; // gread distance: weight not important
+                double directionWeight = Math.abs(deltaAngle) / Math.PI;
+                directionWeight = Math.cos(directionWeight * Math.PI) / 2.0 + 0.5;
+                directionWeight = Math.pow(directionWeight, 4);
+                directionWeight = 0.9 * directionWeight + 0.1;
+                if (distance < model.sectorSize && directionWeight < 0.25) {
+                    directionWeight = 0;
+                }
 
-                double sumFoodNear = model.foods.values().stream().mapToDouble(foodNear -> foodNear.getSize() / (model.sectorSize / 8 + Math.sqrt((food.x - foodNear.x) * (food.x - foodNear.x) + (food.y - foodNear.y) * (food.y - foodNear.y)))).sum();
+                MapPoint goodFoodAngle = new MapPoint(16, new Color(0, 0xFF, 0x80, (int) (sizeWeight * 0xFF)));
+                goodFoodAngle.x = food.x;
+                goodFoodAngle.y = food.y;
+                goodFoodAngle.visible = true;
+                mapPoints.add(goodFoodAngle);
 
-                double weight = Math.pow(food.getSize(), 2) * Math.pow(distance + model.sectorSize / 16, -6) * Math.pow(directionWeight, 4) * Math.pow(sumFoodNear, 1);
+//                MapPoint goodFoodDistance = new MapPoint(16, new Color(0xFF, 0, 0x80, (int) (distanceWeight * 0xFF)));
+//                goodFoodDistance.x = food.x;
+//                goodFoodDistance.y = food.y;
+//                goodFoodDistance.visible = true;
+//                mapPoints.add(goodFoodDistance);
+                double weight = sizeWeight * distanceWeight * directionWeight; //TODO: sperre nach hinten, gegner dazwischen
+                // zwei haufen: wähle einen, nicht die mitte!
+
                 sumX += food.x * weight;
                 sumY += food.y * weight;
                 sumWeight += weight;
